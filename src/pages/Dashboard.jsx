5ba1907721e11d6a297bd5d70a9db5cd94ead724
Sumant9761 <sumantkumaryadav7037@gmail.com>
2026-07-03 15:57:32 +0530

    Modals functionality done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Cards from '../components/Cards'
-import { Modal } from 'antd';
 import AddExpenseModal from '../components/Modals/addExpense';
 import AddIncomeModal from '../components/Modals/addIncome';
+import { addDoc, collection, getDocs, query } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import { toast } from 'react-toastify';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import moment from 'moment';
+
 
 
 const Dashboard = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [user] = useAuthState(auth);
   const[isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const[isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [totalBalance, setTotalBalance] = useState(0);
 
   const showExpenseModal = () => {
     setIsExpenseModalVisible(true);
@@ -26,29 +37,108 @@ const Dashboard = () => {
     setIsIncomeModalVisible(false);
   }
 
-  const onFinish =(values, type) => {
-    console.log("On finish", values, type);
-    
+  const onFinish = (values, type) => {
+    const newTransaction = {
+      type: type,
+      date: moment(values.date).format("YYYY-MM-DD"),
+      amount: parseFloat(values.amount),
+      tag: values.tag,
+      name: values.name,
+    }
+    addTransaction(newTransaction);
+  }
+
+  async function addTransaction(transaction){
+    //Add the doc
+    try{
+      const docRef = await addDoc(
+        collection(db, `users/${user.uid}/transactions`),
+        transaction
+      )
+      console.log("Document written with ID: ", docRef.id);
+      toast.success("Transaction Added!")
+      let newArr = transactions;
+      newArr.push(transaction);
+      setTransactions(newArr);
+      calculateBalance();
+    }catch(e){
+      console.error("Error adding document: ", e)
+      toast.error("Couldn't add transaction")
+    }
+  }
+
+  useEffect(() => {
+    // Get all docs from collection
+    if (user) {
+      fetchtransactions();
+    }
+  }, [user])
+
+  useEffect(() => {
+    calculateBalance();
+  }, [transactions])
+
+  const calculateBalance = () => {
+    let incomeTotal = 0;
+    let expenseTotal = 0;
+
+    transactions.forEach((transaction) => {
+      if(transaction.type === 'income'){
+        incomeTotal += transaction.amount;
+      }else{
+        expenseTotal += transaction.amount;
+      }
+    })
+
+    setIncome(incomeTotal);
+    setExpense(expenseTotal);
+    setTotalBalance(incomeTotal - expenseTotal);
+  }
+
+  async function fetchtransactions(){
+    setLoading(true);
+    if(user){
+      const q = query(collection(db, `users/${user.uid}/transactions`));
+      const querySnapshot = await getDocs(q);
+      let transactionsArray = [];
+      querySnapshot.forEach((doc) => {
+        //doc.data() is never undefined for query doc snapshots
+        transactionsArray.push(doc.data());
+      })
+      setTransactions(transactionsArray);
+      toast.success("Transactions Fetched!");
+    }
+    setLoading(false);
   }
 
 
   return (
     <div>
       <Header />
-      <Cards 
-        showExpenseModal={showExpenseModal}
-        showIncomeModal={showIncomeModal}
-      />
-      <AddExpenseModal 
-        isExpenseModalVisible={isExpenseModalVisible}
-        handleExpenseCancel={handleExpenseCancel}
-        onFinish={onFinish}
-      />
-      <AddIncomeModal 
-        isIncomeModalVisible={isIncomeModalVisible}
-        handleIncomeCancel={handleIncomeCancel}
-        onFinish={onFinish}
-      />
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Cards 
+            income={income}
+            expense={expense}
+            totalBalance={totalBalance}
+            showExpenseModal={showExpenseModal}
+            showIncomeModal={showIncomeModal}
+          />
+          <AddExpenseModal 
+            isExpenseModalVisible={isExpenseModalVisible}
+            handleExpenseCancel={handleExpenseCancel}
+            onFinish={onFinish}
+          />
+          <AddIncomeModal 
+            isIncomeModalVisible={isIncomeModalVisible}
+            handleIncomeCancel={handleIncomeCancel}
+            onFinish={onFinish}
+          />
+        </>
+        )
+      }
     </div>
   )
 }
